1cb693128438d18131804f3271fa6d57719e29f2
Jackyyysue <jmfurog00473@usep.edu.ph>
2024-09-20 12:04:17 +0800

    Update index.php

--- php-login/index.php
+++ php-login/index.php
@@ -47,6 +47,11 @@ unset($_SESSION['error_message']);
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link
+      rel="icon"
+      href="../assets/img/ClinicaLog.ico"
+      type="image/x-icon"
+    />
     <title>CIS</title>
     <link rel="stylesheet" type="text/css" href="../css/style.css">
 </head>
