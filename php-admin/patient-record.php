c8d22b1e4608cf6c8bf727d2e218c8bd7f25ce86
Jackyyysue <jmfurog00473@usep.edu.ph>
2024-09-20 17:22:45 +0800

    update php file

--- php-admin/patient-record.php
+++ php-admin/patient-record.php
@@ -1,21 +1,3 @@
-<?php
-session_start();
-include('../database/config.php');
-include('../php/user.php');
-
-if (!isset($_SESSION['logged_in']) || $_SESSION['logged_in'] !== true) {
-    header('Location: login.php'); 
-    exit;
-}
-
-$db = new Database();
-$conn = $db->getConnection();
-
-$user = new User($conn); 
-$user_id = $_SESSION['user_id'];
-
-?>
-
 <!DOCTYPE html> 
 <html lang="en">
 <head>
@@ -64,7 +46,7 @@ $user_id = $_SESSION['user_id'];
   </style>
 </head>
 <body>
-    <div class="wrapper">
+      <div class="wrapper">
         <!-- Sidebar -->
         <div class="sidebar" id="sidebar"></div>
         <!-- End Sidebar -->
@@ -73,12 +55,314 @@ $user_id = $_SESSION['user_id'];
             <div class="main-header" id="header"></div>
             <!-- Main Content -->
             <div class="container" id="content">
-                <h1>
-                Patient Record
-                </h1>
+            <div class="page-inner">
+              <div class="row">
+                <div class="col-md-12">
+                  <div class="card">
+                    <div class="card-header">
+                      <div class="d-flex align-items-center">
+                        <h4 class="card-title">Medicine</h4>
+                        <button
+                          class="btn btn-primary btn-round ms-auto"
+                          data-bs-toggle="modal"
+                          data-bs-target="#addMedModal"
+                        >
+                          <i class="fa fa-plus"></i>
+                          Add Medicine
+                        </button>
+                      </div>
+                    </div>
+                    <div class="card-body">
+                      <!-- Modal -->
+                      <div
+                        class="modal fade"
+                        id="addMedModal"
+                        tabindex="-1"
+                        role="dialog"
+                        aria-hidden="true"
+                      >
+                        <div class="modal-dialog" role="document">
+                          <div class="modal-content">
+                            <div class="modal-header border-0">
+                              <h5 class="modal-title">
+                                <span class="fw-mediumbold"> New</span>
+                                <span class="fw-light"> Medicine </span>
+                              </h5>
+                              <button
+                                type="button"
+                                class="close"
+                                data-dismiss="modal"
+                                aria-label="Close"
+                              >
+                                <span aria-hidden="true">&times;</span>
+                              </button>
+                            </div>
+                            <div class="modal-body">
+                              <p class="small">
+                              </p>
+                              <form class="form" action="medicinecontrol.php" method="POST">
+                                <div class="row">
+                                  <div class="col-sm-12">
+                                    <div class="form-group form-group-default">
+                                      <label>Category</label>
+                                      <input
+                                        id="addcategory"
+                                        name="addcategory"
+                                        type="text"
+                                        class="form-control"
+                                        placeholder="fill category"
+                                      />
+                                    </div>
+                                  </div>
+                                  <div class="col-md-6 pe-0">
+                                    <div class="form-group form-group-default">
+                                      <label>Name</label>
+                                      <input
+                                        id="addname"
+                                        name="addname"
+                                        type="text"
+                                        class="form-control"
+                                        placeholder="fill name"
+                                      />
+                                    </div>
+                                  </div>
+                                  <div class="col-md-6">
+                                    <div class="form-group form-group-default">
+                                      <label>Quantity</label>
+                                      <input
+                                        id="addquantity"
+                                        name="addquantity"
+                                        type="number"
+                                        class="form-control"
+                                        placeholder="fill quantity"
+                                      />
+                                    </div>
+                                  </div>
+                                  <div class="col-md-6 pe-0">
+                                    <div class="form-group form-group-default">
+                                      <label>Dosage Strength</label>
+                                      <input
+                                        id="addDS"
+                                        name="addDS"
+                                        type="text"
+                                        class="form-control"
+                                        placeholder="fill dosage strength"
+                                      />
+                                    </div>
+                                  </div>
+                                  <div class="col-md-6">
+                                    <div class="form-group form-group-default">
+                                      <label>Expiration Date</label>
+                                      <input
+                                        id="addED"
+                                        name="addED"
+                                        type="date"
+                                        class="form-control"
+                                        placeholder="fill expiration date"
+                                      />
+                                    </div>
+                                  </div>
+                                </div>
+                                <div class="modal-footer border-0">
+                                  <button
+                                    type="submit"
+                                    name="addMedicine"
+                                    class="btn btn-primary"
+                                  >
+                                    Add
+                                  </button>
+                                  <button
+                                    type="button"
+                                    class="btn btn-danger"
+                                    data-bs-dismiss="modal"
+                                  >
+                                    Close
+                                  </button>
+                                </div>
+                              </form>
+                            </div>
+                          </div>
+                        </div>
+                      </div>
+
+                      <div
+                        class="modal fade"
+                        id="editMedModal"
+                        tabindex="-1"
+                        role="dialog"
+                        aria-hidden="true"
+                      >
+                        <div class="modal-dialog" role="document">
+                          <div class="modal-content">
+                            <div class="modal-header border-0">
+                              <h5 class="modal-title">
+                                <span class="fw-mediumbold"> Edit</span>
+                                <span class="fw-light"> Medicine </span>
+                              </h5>
+                              <button
+                                type="button"
+                                class="close"
+                                data-bs-dismiss="modal"
+                                aria-label="Close"
+                                id="edit-exit"
+                              >
+                                <span aria-hidden="true">&times;</span>
+                              </button>
+                            </div>
+                            <div class="modal-body">
+                              <form id="editForm" action="medicinecontrol.php" method="POST">
+                                <div class="row">
+                                <div class="col-sm-12">
+                                    <div class="form-group form-group-default">
+                                      <label>ID</label>
+                                      <input
+                                        id="editid"
+                                        name="editid"
+                                        type="text"
+                                        class="form-control"
+                                        placeholder="fill id" readonly
+                                      />
+                                    </div>
+                                  </div>
+                                <div class="col-sm-12">
+                                    <div class="form-group form-group-default">
+                                      <label>Category</label>
+                                      <input
+                                        id="editcategory"
+                                        name="editcategory"
+                                        type="text"
+                                        class="form-control"
+                                        placeholder="fill category"
+                                      />
+                                    </div>
+                                  </div>
+                                  <div class="col-md-6 pe-0">
+                                    <div class="form-group form-group-default">
+                                      <label>Name</label>
+                                      <input
+                                        id="editname"
+                                        name="editname"
+                                        type="text"
+                                        class="form-control"
+                                        placeholder="fill name"
+                                      />
+                                    </div>
+                                  </div>
+                                  <div class="col-md-6">
+                                    <div class="form-group form-group-default">
+                                      <label>Quantity</label>
+                                      <input
+                                        id="editquantity"
+                                        name="editquantity"
+                                        type="number"
+                                        class="form-control"
+                                        placeholder="fill quantity"
+                                      />
+                                    </div>
+                                  </div>
+                                  <div class="col-md-6 pe-0">
+                                    <div class="form-group form-group-default">
+                                      <label>Dosage Strength</label>
+                                      <input
+                                        id="editDS"
+                                        name="editDS"
+                                        type="text"
+                                        class="form-control"
+                                        placeholder="fill dosage strength"
+                                      />
+                                    </div>
+                                  </div>
+                                  <div class="col-md-6">
+                                    <div class="form-group form-group-default">
+                                      <label>Date Added</label>
+                                      <input
+                                        id="editdateadded"
+                                        name="editdateadded"
+                                        type="date"
+                                        class="form-control"
+                                        placeholder="fill date added" readonly
+                                      />
+                                    </div>
+                                  </div>
+                                  <div class="col-md-6">
+                                    <div class="form-group form-group-default">
+                                      <label>Expiration Date</label>
+                                      <input
+                                        id="editED"
+                                        name="editED"
+                                        type="date"
+                                        class="form-control"
+                                        placeholder="fill expiration date"
+                                      />
+                                    </div>
+                                  </div>
+                                </div>
+                                <div class="modal-footer border-0">
+                              <button
+                                type="submit"
+                                class="btn btn-primary"
+                                data-bs-target="updatemedicine"
+                                id="updatemedicine"
+                                name="updatemedicine"
+                              >
+                                Edit
+                              </button>
+        
+                              <button
+                                type="button"
+                                class="btn btn-secondary"
+                                data-bs-dismiss="modal"
+                                id="edit-close"
+                              >
+                                Close
+                              </button>
+                            </div>
+                              </form>
+                            </div>
+                            
+                          </div>
+                        </div>
+                      </div>
+
+                      <div class="table-responsive">
+                        <table
+                          id="add-med"
+                          class="display table table-striped table-hover"
+                        >
+                          <thead>
+                            <tr>
+                              <th>ID</th>
+                              <th>Category</th>
+                              <th>Name</th>
+                              <th>Quantity</th>
+                              <th>Dosage Strength</th>
+                              <th>Date Added</th>
+                              <th>Expiration Date</th>
+                              <th style="width: 10%">Action</th>
+                            </tr>
+                          </thead>
+                          <tfoot>
+                            <tr>
+                              <th>ID</th>
+                              <th>Category</th>
+                              <th>Name</th>
+                              <th>Quantity</th>
+                              <th>Dosage Strength</th>
+                              <th>Date Added</th>
+                              <th>Expiration Date</th>
+                              <th>Action</th>
+                            </tr>
+                          </tfoot>
+                        </table>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
             </div>
+          </div>
         </div>
-    </div>
+      </div>
 
     
     
